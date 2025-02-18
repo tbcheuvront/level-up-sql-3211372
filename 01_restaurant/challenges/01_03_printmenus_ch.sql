0e3f05cd22c7b65dfa80ffc24d8bf8ea9d50ea08
tbcheuvront <tristenhanson89@gmail.com>
2025-02-18 19:01:06 +0000

    modified:   01_restaurant/challenges/01_03_printmenus_ch.sql 	modified:   01_restaurant/challenges/01_03_printmenus_ch.sql

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,38 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+-- This solution I've developed works, but the one in the solution
+-- is more elegant. It avoids the IN clause and excludes columns
+-- which are not to be displayed to customers.
+SELECT
+  *
+FROM
+  Dishes
+ORDER BY
+  Price;
+
+SELECT
+  *
+FROM
+  Dishes
+WHERE
+  Type
+IN
+  ('Appetizer',
+  'Beverage')
+ORDER BY
+  Type;
+
+SELECT
+  *
+From
+  Dishes
+WHERE
+  Type
+IN
+  ('Appetizer',
+  'Dessert',
+  'Main')
+ORDER BY
+  Type;
