d267d2501e9b336ff049f9e7ce2e89ba98eb221e
tbcheuvront <tristenhanson89@gmail.com>
2025-02-18 19:27:40 +0000

    modified:   01_restaurant/challenges/01_05_updateinfo_ch.sql modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,30 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+
+-- It probably would've been easier to just use the customerID once I
+-- identified it, but this way allows me to use just the information
+-- front of me, without hunting down any further information.
+
+SELECT
+  *
+FROM
+  Customers
+WHERE
+  Address = '27170 6th Ave.';
+
+UPDATE 
+  Customers
+SET
+  Address = '74 Pine St.',
+  City = 'New York',
+  State = 'NY'
+WHERE
+  Address = '27170 6th Ave.' AND City = 'Washington' AND State = 'DC';
+
+SELECT
+  *
+FROM
+  Customers
+WHERE
+  FirstName = 'Taylor' AND LastName = 'Jenkins';
