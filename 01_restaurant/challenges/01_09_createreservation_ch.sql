9bb27e7573c465c1e87612d36e5628b7e17d28c7
tbcheuvront <tristenhanson89@gmail.com>
2025-02-26 17:26:25 +0000

    modified:   01_restaurant/challenges/01_09_createreservation_ch.sql modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,44 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT
+  CustomerID
+FROM
+  Customers
+WHERE
+  LastName = 'McAdams' AND Email = 'smac@kinetecoinc.com';
+
+-- This showed us that the customer is not in the database.
+
+INSERT INTO Customers
+  (FirstName, LastName, Email)
+VALUES
+  ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+
+-- Here we create a CustomerID for Sam McAdams in the system.
+
+SELECT
+  CustomerID
+FROM
+  Customers
+WHERE LastName = 'McAdams';
+
+-- Confirm that McAdams has been added and has a corresponding CustomerID
+
+INSERT INTO Reservations
+  (CustomerID, Date, PartySize)
+VALUES(
+  (SELECT CustomerID
+  FROM Customers
+  WHERE LastName = 'McAdams'),
+  '2022-08-12 1800:00:00',
+  5);
+
+-- Add the reservation. Accidentally did this three times troubleshooting.
+
+SELECT *
+FROM Reservations
+WHERE CustomerID = 102;
+
+-- Confirm reservation is added.
