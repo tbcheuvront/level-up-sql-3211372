65ba1648118d42c95ded9794b8690e89e60d7a5f
tbcheuvront <tristenhanson89@gmail.com>
2025-02-26 16:09:17 +0000

    modified:   .vscode/settings.json 	modified:   01_restaurant/challenges/01_06_removeinfo_ch.sql

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,22 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT
+  CustomerID
+FROM
+  Customers
+WHERE
+  LastName = 'Norby' OR FirstName = 'Norby';
+
+SELECT
+  ReservationID
+FROM
+  Reservations
+WHERE
+  CustomerID = 64 AND Date LIKE '2022-07%';
+
+DELETE FROM
+  Reservations
+WHERE
+  ReservationID = '2000';
