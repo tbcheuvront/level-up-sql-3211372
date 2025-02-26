35a30a46919b76e5365ca6de9a1b83aa631caea0
tbcheuvront <tristenhanson89@gmail.com>
2025-02-26 17:04:38 +0000

    modified:   01_restaurant/challenges/01_08_findreservation_ch.sql

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,14 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT
+  Customers.FirstName AS 'First Name',
+  Customers.LastName AS 'Last Name',
+  Reservations.PartySize AS 'Party Size',
+  Reservations.Date AS 'Date'
+FROM
+  Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE
+  Customers.LastName LIKE 'St%' AND Reservations.Date LIKE '%-06-14%';
