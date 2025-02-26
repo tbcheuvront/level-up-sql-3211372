fc3bcca2221e9a105d00da3e2e658a2707a4db85
tbcheuvront <tristenhanson89@gmail.com>
2025-02-26 17:48:37 +0000

    modified:   01_restaurant/challenges/01_10_takeorder_ch.sql modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,51 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT
+  FirstName,
+  LastName,
+  CustomerID
+FROM Customers
+WHERE LastName = 'Hundey' AND Address = '6939 Elka Place';
+
+-- Confirm customer is in database, identify CustomerID as 70.
+
+INSERT INTO Orders
+  (CustomerID, OrderDate)
+VALUES
+  (70, '2022-09-20 14:00:00');
+
+-- Insert order into Orders.
+
+SELECT
+  OrderID
+FROM
+  Orders
+WHERE CustomerID = 70
+ORDER BY OrderID Desc;
+
+-- Identify OrderID as 1001.
+
+INSERT INTO OrdersDishes
+  (OrderID, DishID)
+VALUES
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'House Salad')),
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers')),
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+-- Add items ordered to the OrdersDishes table.
+
+SELECT *
+FROM Dishes
+JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID
+WHERE OrdersDishes.OrderID = 1001;
+
+-- Confirm items added and correctly affiliated with OrderID.
+
+SELECT SUM(Dishes.Price)
+FROM Dishes
+JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID
+WHERE OrdersDishes.OrderID = 1001;
+
+-- Find the sum of the order cost.
