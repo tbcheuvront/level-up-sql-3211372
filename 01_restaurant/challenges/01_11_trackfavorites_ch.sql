d4836342cca20c9753d79c2d48b7b9fd6185deae
tbcheuvront <tristenhanson89@gmail.com>
2025-02-27 18:57:21 +0000

    modified:   01_restaurant/challenges/01_11_trackfavorites_ch.sql modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,24 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+UPDATE 
+  Customers
+SET FavoriteDish =
+  (SELECT
+    DishID
+  FROM
+    Dishes
+  WHERE
+    Name = 'Quinoa Salmon Salad')
+WHERE
+  FirstName = 'Cleo' AND LastName = 'Goldwater';
+
+SELECT
+  FirstName,
+  LastName,
+  FavoriteDish
+FROM
+  Customers
+WHERE
+  FirstName = 'Cleo' AND LastName = 'Goldwater';
